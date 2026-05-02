7e4dbcf34c9a4aad91d110b8563554e180952c35
Rajat <raj.men00@gmail.com>
2026-05-02 15:48:42 +0800

    basic snake movement single block

---
+++ static/script.js
@@ -0,0 +1,76 @@
+const canvas = document.getElementById("cnvs");
+canvas.width = 1400;
+canvas.height = 600;
+
+const ctx = canvas.getContext("2d");
+
+const gridSize = 20;
+
+let snake = [{x:5, y:5}];
+
+let dx = 0;
+let dy = 0;
+let started = false;
+
+function gameLoop() {
+    update();
+    draw();
+}
+
+setInterval(gameLoop, 100);
+
+
+function update() {
+    if (!started) return;
+
+    const head = {
+        x: snake[0].x + dx,
+        y: snake[0].y + dy
+    };
+    
+    snake.unshift(head);
+    snake.pop();
+
+}
+
+function draw() {
+    
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+
+    ctx.fillStyle = "black";
+
+    snake.forEach(seg => {
+        ctx.fillRect(
+            seg.x*gridSize,
+            seg.y*gridSize,
+            gridSize,
+            gridSize
+        );
+    });
+}
+
+document.addEventListener("keydown", (e) => {
+
+    if (!started) {
+        started = true; 
+    }
+
+    if (e.key === "ArrowUp" && dy === 0) {
+        dx = 0; dy = -1;
+    }
+    else if (e.key === "ArrowDown" && dy === 0) {
+        dx = 0; dy = 1;
+    }
+    else if (e.key === "ArrowLeft" && dx === 0) {
+        dx = -1; dy = 0;
+    }
+    else if (e.key === "ArrowRight" && dx === 0) {
+        dx = 1; dy = 0;
+    }
+});
+
+
+
+
+
+
